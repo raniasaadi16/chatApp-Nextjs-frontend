14990a1788153e72942c9317826001dee354e631
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2022-03-14 14:26:16 +0100

    update middleware func

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,38 +1,37 @@
 import { NextRequest, NextResponse } from 'next/server'
 //import { checkAuthToken } from '../utils/Apicalls'
 
-export default async function middleware() {
-    //const { pathname } = NextRequest.nextUrl
-    console.log(NextRequest)
+export default async function middleware(req) {
+    const { pathname } = req.nextUrl
 
     
-    // try{
-    //     const res = await fetch(`https://chat-app-rania.herokuapp.com/api/users/isLoggedin/${NextRequest.cookies.jwt}`, {
-    //         method: 'GET',
-    //         credentials:'include',
-    //         headers: {
-    //             'Access-Control-Allow-Credentials': true
-    //         },
-    //     })
-    //     const data = await res.json()
-    //     if(!res.ok){
-    //         throw data 
-    //     }
-    //     if(data.data){
-    //         console.log(data.data)
-    //         if(!data.data.isAuth && pathname !== '/login' && pathname !== '/signup'){
-    //             return NextResponse.redirect('/login')
-    //         }
-    //         if(data.data.isAuth && pathname === '/login'){
-    //             return NextResponse.redirect('/')
-    //         }
-    //         if(data.data.isAuth && pathname === '/signup'){
-    //             return NextResponse.redirect('/')
-    //         }
-    //     }
-    // }catch(err){
-    //     return console.log(err)
-    // }
+    try{
+        const res = await fetch(`https://chat-app-rania.herokuapp.com/api/users/isLoggedin/${req.cookies.jwt}`, {
+            method: 'GET',
+            credentials:'include',
+            headers: {
+                'Access-Control-Allow-Credentials': true
+            },
+        })
+        const data = await res.json()
+        if(!res.ok){
+            throw data 
+        }
+        if(data.data){
+            console.log(data.data)
+            if(!data.data.isAuth && pathname !== '/login' && pathname !== '/signup'){
+                return NextResponse.redirect('/login')
+            }
+            if(data.data.isAuth && pathname === '/login'){
+                return NextResponse.redirect('/')
+            }
+            if(data.data.isAuth && pathname === '/signup'){
+                return NextResponse.redirect('/')
+            }
+        }
+    }catch(err){
+        return console.log(err)
+    }
 
     
    

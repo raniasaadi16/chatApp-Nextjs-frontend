2250a5b02556dbff2e6888def24d2af366004d7d
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2022-03-14 14:14:53 +0100

    remove ts

---
+++ pages/_middleware.js
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from 'next/server'
+//import { checkAuthToken } from '../utils/Apicalls'
+
+export default async function middleware() {
+    //const { pathname } = NextRequest.nextUrl
+    console.log(NextRequest)
+
+    
+    // try{
+    //     const res = await fetch(`https://chat-app-rania.herokuapp.com/api/users/isLoggedin/${NextRequest.cookies.jwt}`, {
+    //         method: 'GET',
+    //         credentials:'include',
+    //         headers: {
+    //             'Access-Control-Allow-Credentials': true
+    //         },
+    //     })
+    //     const data = await res.json()
+    //     if(!res.ok){
+    //         throw data 
+    //     }
+    //     if(data.data){
+    //         console.log(data.data)
+    //         if(!data.data.isAuth && pathname !== '/login' && pathname !== '/signup'){
+    //             return NextResponse.redirect('/login')
+    //         }
+    //         if(data.data.isAuth && pathname === '/login'){
+    //             return NextResponse.redirect('/')
+    //         }
+    //         if(data.data.isAuth && pathname === '/signup'){
+    //             return NextResponse.redirect('/')
+    //         }
+    //     }
+    // }catch(err){
+    //     return console.log(err)
+    // }
+
+    
+   
+    return NextResponse.next()
+}

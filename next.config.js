e75a81a58a3b4bcb5b7857da675d4aecba4dd986
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2023-04-16 22:15:12 +0200

    edit next config

--- next.config.js
+++ next.config.js
@@ -2,14 +2,5 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com','lh3.googleusercontent.com','platform-lookaside.fbsbx.com']
   },
-  swcMinify : false,
-  distDir: "build",
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://chat-app-rania.herokuapp.com/api/:path*',
-      },
-    ]
-  },
+  swcMinify : true
 }

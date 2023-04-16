83726588148e2b4d75bc67f2b99b4d39093d0053
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2023-04-16 22:04:50 +0200

    prepare for deployment

--- components/chat/AddChannel.jsx
+++ components/chat/AddChannel.jsx
@@ -49,3 +49,4 @@ export default function AddChannel({setisOpen}) {
       </div>
   )
 }
+
--- utils/Apicalls.js
+++ utils/Apicalls.js
@@ -1,5 +1,6 @@
-const url = 'https://chat-app-rania.herokuapp.com/api'
+// const url = 'https://chat-app-rania.herokuapp.com/api'
 // const url = 'http://localhost:5000/api'
+const url = 'https://chat-app-nextjs-backend-zjw3m11h2-raniasaadi16.vercel.app'
 // const url = "https://chat-app-nextjs-backend.vercel.app/api"
 //const url = "https://chat-app-nextjs-backend.vercel.app/api"
 export const checkAuthToken = async (token) => {
